666e2d2858656ec2b75cc1bea2f9fbd679776a5a
Podovzhniy <podovzhniy@gmail.com>
2017-03-03 13:45:48 +0200

    Init

---
+++ BowlingGameTest/GameTest.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using BowlingGame;
+
+namespace BowlingGameTest
+{
+    [TestClass]
+    public class GameTest
+    {
+        private TestContext testContextInstance;
+        public TestContext TestContext
+        {
+            get { return testContextInstance; }
+            set { testContextInstance = value; }
+        }
+
+        private Game g;
+        
+        [TestInitialize]
+        public void Initialize()
+        {
+            g = new Game();
+        }
+
+
+        [TestMethod]
+        public void testGoodGame()
+        {
+            RollMany(20, 0);
+            
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void testAllOnes()
+        {
+            RollMany(20, 0);
+
+            Assert.AreEqual(0, g.Score());
+        }
+
+        [TestMethod]
+        public void testOneSpare()
+        {
+            RollSpare();
+            g.Roll(3);
+            RollMany(17, 0);
+
+            Assert.AreEqual(16, g.Score());
+        }
+
+        [TestMethod]
+        public void testOneStrike()
+        {
+            RollStrike();
+            g.Roll(3);
+            g.Roll(4);
+            RollMany(16, 0);
+
+            Assert.AreEqual(24, g.Score());
+        }
+
+        private void RollSpare()
+        {
+            g.Roll(5);
+            g.Roll(5);
+        }
+
+        private void RollStrike()
+        {
+            g.Roll(10);
+        }
+
+        private void RollMany(int rolls, int pins)
+        {
+            for (int i = 0; i < rolls; i++)
+            {
+                g.Roll(pins);
+            }
+        }
+    }
+}
